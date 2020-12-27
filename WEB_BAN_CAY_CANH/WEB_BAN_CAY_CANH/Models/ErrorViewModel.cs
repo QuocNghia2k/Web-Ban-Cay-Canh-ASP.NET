9d63a4e6320c5f4379a03f53c92ff878e0f75e22
Hồ Quốc Nghĩa <64093081+apeo8a123@users.noreply.github.com>
2020-12-27 11:39:39 +0700

    add project
    add project

---
+++ WEB_BAN_CAY_CANH/WEB_BAN_CAY_CANH/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WEB_BAN_CAY_CANH.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
